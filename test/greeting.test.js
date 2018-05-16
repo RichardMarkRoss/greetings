b1582b05a0af55765cecc29246085cb7fb950c58
RichardMarkRoss <cptrichkidprice@gmail.com>
2018-05-16 15:32:14 +0200

    commit

--- test/greeting.test.js
+++ test/greeting.test.js
@@ -18,23 +18,25 @@ describe('the greetings function basic test', function(){
 });
 describe('the greetings function testing the counter', function(){
     it('greetings function should test is incremented by 1', function(){
-        var storedUsers = [{'Richard':0}]
 
-        var theGreetingsVar = GreetingsFactory(storedUsers);
+        var theGreetingsVar = GreetingsFactory([{'Richard':0}]);
 
         assert.equal(theGreetingsVar.TheGreetCounter(),1);
     });
     it('greetings function should test if the same name should stay one', function(){
-        var storedUsers = [{'Richard':0},]
 
-        var theGreetingsVar = GreetingsFactory(storedUsers);
+        var theGreetingsVar = GreetingsFactory([{'Richard':0},]);
 
         assert.equal(theGreetingsVar.TheGreetCounter(),1);
+
+        var theGreetingsVar = GreetingsFactory([{'Richard':0},]);
+
+        assert.equal(theGreetingsVar.TheGreetCounter(),1);
+
     });
     it('greetings function should if the it has three different names, should return three', function(){
-        var storedUsers = [{'Aya':0}, {'mike':0}, {'Sibo':0}]
 
-        var theGreetingsVar = GreetingsFactory(storedUsers);
+        var theGreetingsVar = GreetingsFactory([{'Aya':0}, {'mike':0}, {'Sibo':0}]);
 
         assert.equal(theGreetingsVar.TheGreetCounter(),3);
     });
